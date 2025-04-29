d4dfe4cdd5a401d46b76ba89888e8d5c5dbefd95
aalibrahimi <166450703+aalibrahimi@users.noreply.github.com>
2025-04-28 17:34:53 -0700

    added locale, notfound, routing, async, and nextinclientprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
-"use client";
+
 
 import "./Styles/globals.css";
 import "./Styles/mediaSizing.css";
-
-
+import { notFound } from "next/navigation";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { routing } from "@/i18n/routing";
 import { Navbar } from "@/MyComponents/Navbar";
 import Footer from "@/MyComponents/Footer";
+
 
 // Need to make layout server-side file
 // export const metadata = {
@@ -13,17 +15,34 @@ import Footer from "@/MyComponents/Footer";
 //   description: 'At CodeWithAli, we empower individuals and businesses with the tools, knowledge, and applications needed to thrive in the digital age',
 // }
 
-export default function RootLayout({
+export const metadata = {
+  title: "CodeWithAli",
+  description: "We Create, Build, Designs Websites and apps to boost your business",
+};
+
+
+export default async function RootLayout({
   children,
-}: Readonly<{
+  params,
+}: {
   children: React.ReactNode;
-}>) {
+  params: Promise<{ locale: string }>;
+}) {
+
+   // Ensure that the incoming `locale` is valid
+   const { locale } = await params;
+   if (!hasLocale(routing.locales, locale)) {
+     notFound();
+   }
+
   return (
     <html lang="en">
       <body className="bg-black text-red-200">
+      <NextIntlClientProvider>
        <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
